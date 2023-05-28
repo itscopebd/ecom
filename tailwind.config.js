5e24da721a27aaea30a87d1d2b6fb0c6aaa4947b
itscopebd <itscopebd@gmail.com>
2023-05-28 22:16:29 +0600

    Modify all files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppies:"'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
